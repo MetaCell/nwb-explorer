83a26345d2f9ccb1058c734c4e9628ac6afeac49
johnidol <giovanni.idili@gmail.com>
2018-04-05 00:31:33 +0100

    some changes to tests

--- tests/tests.py
+++ tests/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from datetime import datetime
+import model as pygeppetto
+from model.model_factory import GeppettoModelFactory
 
 import numpy as np
 
@@ -31,22 +33,20 @@ class PyNWBTestCase(TestCase):
 
 class PyNWBAnotherTestCase(TestCase):
 
-    nwbfile = None
+    factory = GeppettoModelFactory()
 
     def setUp(self):
         start_time = datetime(2017, 4, 3, 11, 0, 0)
         create_date = datetime(2017, 4, 15, 12, 0, 0)
 
         # create your NWBFile object
-        self.nwbfile = NWBFile('PyNWB Sample File', 'A simple NWB file', 'NWB_test', start_time,
+        nwbfile = NWBFile('PyNWB Sample File', 'A simple NWB file', 'NWB_test', start_time,
                         file_create_date=create_date)
-
-
-    def test_create_complicated_NWB_file(self):
+        
         # create acquisition metadata
         optical_channel = OpticalChannel('test_optical_channel', 'optical channel source',
                                         'optical channel description', '3.14')
-        imaging_plane = self.nwbfile.create_imaging_plane('test_imaging_plane',
+        imaging_plane = nwbfile.create_imaging_plane('test_imaging_plane',
                                                     'ophys integration tests',
                                                     optical_channel,
                                                     'imaging plane description',
@@ -58,10 +58,10 @@ class PyNWBAnotherTestCase(TestCase):
         image_series = TwoPhotonSeries(name='test_iS', source='a hypothetical source', dimension=[2],
                                     external_file=['images.tiff'], imaging_plane=imaging_plane,
                                     starting_frame=[1, 2, 3], format='tiff', timestamps=list())
-        self.nwbfile.add_acquisition(image_series)
+        nwbfile.add_acquisition(image_series)
 
 
-        mod = self.nwbfile.create_processing_module('img_seg_example', 'ophys demo', 'an example of writing Ca2+ imaging data')
+        mod = nwbfile.create_processing_module('img_seg_example', 'ophys demo', 'an example of writing Ca2+ imaging data')
         img_seg = ImageSegmentation('a toy image segmentation container')
         mod.add_data_interface(img_seg)
         ps = img_seg.create_plane_segmentation('integration test PlaneSegmentation', 'plane segmentation description',
@@ -96,37 +96,16 @@ class PyNWBAnotherTestCase(TestCase):
         # write data
         nwb_path = './test_data/nwb_test_file.nwb'
         with NWBHDF5IO(nwb_path, 'w') as io:
-            io.write(self.nwbfile)
-
-        # read data back in
-        io = NWBHDF5IO(nwb_path, 'r')
-        self.nwbfile = io.read()
-
-        # get the processing module
-        mod = self.nwbfile.get_processing_module('img_seg_example')
-
-        # get the PlaneSegmentation from the ImageSegmentation data interface
-        ps = mod['ImageSegmentation'].get_plane_segmentation()
-        img_mask1 = ps.get_image_mask(0)
-        pix_mask1 = ps.get_pixel_mask(0)
-        img_mask2 = ps.get_image_mask(1)
-        pix_mask2 = ps.get_pixel_mask(1)
-
-        # get the RoiResponseSeries from the Fluorescence data interface
-        rrs = mod['Fluorescence'].get_roi_response_series()
-        # get the data...
-        rrs_data = rrs.data
-        rrs_timestamps = rrs.timestamps
-        # and now do something cool!
+            io.write(nwbfile)
 
     def test_open_NWB_file_and_inspect_data(self):
         nwb_path = './test_data/nwb_test_file.nwb'
         # read data back in
         io = NWBHDF5IO(nwb_path, 'r')
-        self.nwbfile = io.read()
+        nwbfile = io.read()
 
         # get the processing module
-        mod = self.nwbfile.get_processing_module('img_seg_example')
+        mod = nwbfile.get_processing_module('img_seg_example')
 
         # get the RoiResponseSeries from the Fluorescence data interface
         rrs = mod['Fluorescence'].get_roi_response_series()
@@ -136,3 +115,7 @@ class PyNWBAnotherTestCase(TestCase):
 
         self.assertTrue(np.array_equal(rrs_data[()], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]))
         self.assertTrue(np.array_equal(rrs_timestamps[()], [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]))
+
+        nwbType = pygeppetto.CompositeType(id=str('nwb'), name=str('nwb'), abstract= False)
+        ts_val = self.factory.createTimeSeries('myTimeSeriesValue', rrs_data[()].all())
+        nwbType.variables.append(self.factory.createStateVariable('timeSeriesVariable', ts_val))
