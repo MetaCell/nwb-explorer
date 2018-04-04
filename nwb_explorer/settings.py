629e497fa2f8f395db91853d12c18fe7226645ac
johnidol <giovanni.idili@gmail.com>
2018-04-04 17:44:25 +0100

    added pynwb examples / tests

--- nwb_explorer/settings.py
+++ nwb_explorer/settings.py
@@ -95,12 +95,12 @@ WSGI_APPLICATION = 'nwb_explorer.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Password validation
