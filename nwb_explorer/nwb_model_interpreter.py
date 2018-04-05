34805bbddd8aa74696fcfb00d8dfec7f66292e34
johnidol <giovanni.idili@gmail.com>
2018-04-05 01:29:20 +0100

    model interpreter pull out a bunch of time variables from nwb sample file

--- nwb_explorer/nwb_model_interpreter.py
+++ nwb_explorer/nwb_model_interpreter.py
@@ -20,10 +20,30 @@ class NWBModelInterpreter(ModelInterpreter):
 
         factory = GeppettoModelFactory()
         geppetto_model = factory.createGeppettoModel('GepettoModel')
-        nwb_geppetto_library = pygeppetto.GeppettoLibrary(
-            name='nwblib')
+        nwb_geppetto_library = pygeppetto.GeppettoLibrary(name='nwblib')
         geppetto_model.libraries.append(nwb_geppetto_library)
         model = GeppettoModelSerializer().serialize(geppetto_model)
+
+        # read data 
+        io = NWBHDF5IO(url, 'r')
+        nwbfile = io.read()
+
+        # get the processing module
+        mod = nwbfile.get_processing_module('ophys')
+
+        # get the RoiResponseSeries from the Fluorescence data interface
+        # get the data...
+        rrs = mod['DfOverF'].get_roi_response_series()
+        rrs_data = rrs.data
+        rrs_timestamps = rrs.timestamps
+
+        nwbType = pygeppetto.CompositeType(id=str('nwb'), name=str('nwb'), abstract= False)
+        ts_val1 = self.factory.createTimeSeries('myTimeSeriesValue', rrs_data[()][0].all())
+        nwbType.variables.append(self.factory.createStateVariable('timeSeriesVariable', ts_val1))
+        ts_val2 = self.factory.createTimeSeries('myTimeSeriesValue', rrs_data[()][1].all())
+        nwbType.variables.append(self.factory.createStateVariable('timeSeriesVariable', ts_val2))
+        time = self.factory.createTimeSeries('myTimeSeriesValue', rrs_timestamps[()].all())
+        geppetto_model.variables.append(self.factory.createStateVariable('time', time))
 
         return model
 
