5c32fb013f5545192e99ac4f63194a9b4881df73
johnidol <giovanni.idili@gmail.com>
2018-04-25 21:39:40 +0100

    fix import

--- nwb_explorer/urls.py
+++ nwb_explorer/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.conf.urls import include,url
 from django.contrib import admin
-from api import views
+from .api import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
