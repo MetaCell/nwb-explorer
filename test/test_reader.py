b4bf0e9f272c8ca0b50392cb966393f6c2e417b3
Filippo Ledda <filippo@metacell.us>
2019-08-19 19:26:54 +0200

    #36 revised model interpreter ok. Tests passing

--- test/test_reader.py
+++ test/test_reader.py
@@ -11,33 +11,7 @@ def uut():
     return NWBReader(nwbfile)
 
 
-def _test_big_file(uut):
-    '''
-    Testing a real file here for dev purposes
-    :return:
-    '''
-    file_path = "/home/user/nwb-explorer-jupyter/test_data/pynwb/YutaMouse41-150903.nwb"
-    uut = NWBReader(file_path)
-    process_module = uut.nwbfile.modules['behavior']
-    interface = process_module.get_data_interface('EightMazePosition_position')
-    children = interface.fields['spatial_series']
-    ts = children['EightMazePosition_linearized_spatial_series']
-    path = uut.extract_time_series_path(ts)
-    assert 4 == len(path)
 
-
-def test_extract_time_series_path(uut):
-    process_module = uut.nwbfile.modules['mod']
-    ts = process_module.get_data_interface('t3')
-    path = uut.extract_time_series_path(ts)
-    assert 2 == len(path)
-    assert 'processing' == path[0]
-    assert 'mod' == path[1]
-
-    ts = uut.nwbfile.get_acquisition('t1')
-    path = uut.extract_time_series_path(ts)
-    assert 1 == len(path)
-    assert 'acquisition' == path[0]
 
 
 def test_retrieve_from_path(uut):
