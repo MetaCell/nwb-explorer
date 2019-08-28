a2467c8eb26ad58efc0b17385336aa07c9865113
rodriguez-facundo <bruseghini.f@gmail.com>
2019-08-28 16:24:26 -0300

    Skip travis cloning pygeppetto development until it is updated with the latest from nwbdev

--- utilities/install.py
+++ utilities/install.py
@@ -45,7 +45,10 @@ def checkout(folder, default_branch, cwdp):
     python_git = subprocess.Popen("git branch -a", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     outstd, errstd = python_git.communicate()
     if branch and branch in str(outstd):
-        subprocess.call(['git', 'checkout', branch], cwd='./')
+        if default_branch == 'nwbdev':
+            subprocess.call(['git', 'checkout', default_branch], cwd='./')
+        else:
+            subprocess.call(['git', 'checkout', branch], cwd='./')
     else:
         subprocess.call(['git', 'checkout', default_branch], cwd='./')
     os.chdir(currentPath)
