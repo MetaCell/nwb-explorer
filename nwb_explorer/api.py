db99a08daa9a0591c2ab03982c903a0450db7845
Filippo Ledda <filippo@metacell.us>
2019-09-09 11:38:09 +0200

    Add pre-compiled notebook with instructions

--- nwb_explorer/api.py
+++ nwb_explorer/api.py
@@ -10,18 +10,51 @@ from . import nwb_data_manager
 
 from pygeppetto.managers import GeppettoManager
 from pygeppetto.managers.geppetto_manager import RuntimeProject
-
+from traitlets import Instance
 cache_model = False
 
 from pygeppetto.services.data_manager import DataManagerHelper
 import logging
 import os
 from notebook.notebook.handlers import get_custom_frontend_exporters
+
 from pygeppetto.services.model_interpreter import get_model_interpreter_from_variable
+
+
+def createNotebook(filename):
+    import nbformat as nbf
+    from nbformat.v4.nbbase import new_notebook
+    from nbformat import sign
+    import codecs
+    nb0 = new_notebook(cells=[nbf.v4.new_markdown_cell("""Welcome to the NWB Explorer!
+--
+
+With this Python console you can programmatically access the loaded data using the [PyNWB Python API](https://pynwb.readthedocs.io/en/stable/).
+
+The loaded NWB:N 2 file can be accessed from the variable `nwbfile`. 
+If you would like to inspect the content of the file using the [NWB Juypyter widgets](https://pypi.org/project/nwbwidgets/) you can use the `show()` function.
+
+To execute a command type it and press `Shift+Enter`. To execute a command and create a new cell press `Alt+Enter`."""),
+                              nbf.v4.new_code_cell('nwbfile')
+                              ], metadata={"kernelspec": {
+        "display_name": "Python 3",
+        "language": "python",
+        "name": "python3"
+    }})
+
+    f = codecs.open(filename, encoding='utf-8', mode='w')
+
+    nbf.write(nb0, f, 4)
+    f.close()
+
+
+
+
 
 
 def get_model_interpreter(runtime_project) -> NWBModelInterpreter:
     return get_model_interpreter_from_variable(runtime_project.model.variables[0])
+
 
 class NWBController:  # pytest: no cover
 
@@ -45,7 +78,6 @@ class NWBController:  # pytest: no cover
     def new_notebook(handler: IPythonHandler, path):
         if not os.path.exists(path):
             logging.info("Creating notebook {}".format(path))
-            from jupyter_geppetto.utils import createNotebook
             createNotebook(path)
         return handler.render_template('notebook.html',
                                        notebook_path=path,
