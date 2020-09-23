8c51c9ef7342798316cdc0b4d1a181292441d0d6
Vidhya Longani <vidhya@metacell.us>
2020-09-23 18:33:17 +0530

    #193 Lintng, new variables for colors

--- webapp/theme.js
+++ webapp/theme.js
@@ -31,8 +31,8 @@ const baseTheme = {
       textTransform: "none",
       fontSize: "14px",
       lineHeight: "24px",
-      fontWeight: '500',
-      letterSpacing: '0.16px',
+      fontWeight: "500",
+      letterSpacing: "0.16px",
       padding: "9px 16px",
     },
   },
@@ -45,71 +45,71 @@ const baseTheme = {
   overrides: {
     MuiInput: {
       input: {
-        outline: 'none !important',
-        border: 'none !important',
-        boxShadow: 'none !important',
-        fontSize: '16px',
+        outline: "none !important",
+        border: "none !important",
+        boxShadow: "none !important",
+        fontSize: "16px",
       },
-      root:{ color: fontColor }
+      root: { color: fontColor },
     },
-    MuiPaper: { root: { color: 'inherit', backgroundColor: bgRegular } },
-    MuiFormControl: { root: { overflow: 'visible' } },
+    MuiPaper: { root: { color: "inherit", backgroundColor: bgRegular } },
+    MuiFormControl: { root: { overflow: "visible" } },
     MuiButton: {
       contained: {
         color: fontColor,
-        backgroundColor: bgInputs
+        backgroundColor: bgInputs,
+        borderRadius: "2px",
       },
       outlined: {
         borderColor: buttonOutlineColor,
-        borderRadius: '16px',
+        borderRadius: "16px",
         color: buttonOutlineColor,
         fontSize: "13px",
         lineHeight: "13px",
-        fontWeight: '400',
-        marginBottom: '9px'
+        fontWeight: "400",
+        marginBottom: "9px",
       },
       containedSecondary: { color: fontColor },
       containedPrimary: { color: fontColor },
     },
     MuiTypography: {
-      color: '#FFF',
-      fontWeight: '400',
+      color: "#FFF",
+      fontWeight: "400",
       h1: {
-        fontSize: '34px',
-        lineHeight: '40px'
+        fontSize: "34px",
+        lineHeight: "40px",
       },
       h2: {
-        fontSize: '20px',
-        lineHeight: '32px',
-        letterSpacing: '0.5px',
-        marginBottom: '41px',
-        color: 'rgba(255,255,255,0.3)',
-        fontWeight: '400'
+        fontSize: "20px",
+        lineHeight: "32px",
+        letterSpacing: "0.5px",
+        marginBottom: "41px",
+        color: "rgba(255,255,255,0.3)",
+        fontWeight: "400",
       },
       h3: {
-        fontSize: '16px',
-        lineHeight: '18px',
-        marginBottom: '11px'
+        fontSize: "16px",
+        lineHeight: "18px",
+        marginBottom: "11px",
       },
       h4: {
-        fontSize: '12px',
-        lineHeight: '20px',
-        letterSpacing: '0.32px',
+        fontSize: "12px",
+        lineHeight: "20px",
+        letterSpacing: "0.32px",
         color: bgInputs,
-        textTransform: 'Uppercase',
-        margin: '22px 0 11px',
+        textTransform: "Uppercase",
+        margin: "22px 0 11px",
       },
       h6: {
-        fontSize: '12px',
-        lineHeight: '20px',
-        marginBottom: '5px',
-        color: primaryColor
+        fontSize: "12px",
+        lineHeight: "20px",
+        marginBottom: "5px",
+        color: primaryColor,
       },
 
-      root: { color: fontColor }
+      root: { color: fontColor },
     },
-
-  }
+  },
 };
 
 export default createMuiTheme(baseTheme);
