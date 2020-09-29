60e8da4f3993faba2a8e45f6c78abd43dad361ef
Vidhya Longani <vidhya@metacell.us>
2020-09-29 21:54:27 +0530

    #193 List View new configuration including the new eye icon and custom menu component

--- webapp/components/configuration/listViewerConfiguration.js
+++ webapp/components/configuration/listViewerConfiguration.js
@@ -1,12 +1,100 @@
-import React from 'react';
-import { GroupComponent, IconComponent, ColorComponent } from "@geppettoengine/geppetto-ui/list-viewer/ListViewer";
+import { GroupComponent, } from "@geppettoengine/geppetto-ui/list-viewer/ListViewer";
+import { FILEVARIABLE_LENGTH } from "../constants";
+import { CustomIconComponent } from "../CustomIconComponent";
+import ListControlsComponent from "../ListMenuComponent";
 
-
-import AddToPlotComponent from './AddToPlotComponent';
-
-import { FILEVARIABLE_LENGTH } from '../constants';
-``
 const conf = [
+  {
+    id: "controls",
+    title: "Controls",
+    customComponent: GroupComponent,
+
+    configuration: [
+      {
+        id: "showPlot",
+        customComponent: CustomIconComponent,
+        visible: entity =>
+          Instances.getInstance(entity.path + ".data")
+            && Instances.getInstance(entity.path + ".timestamps"),
+
+        source: entity => entity,
+        configuration: {
+          action: "clickShowPlot",
+          label: "Plot",
+          tooltip: "Plot time series",
+          color: "#ffffff",
+          defaultColor: entity => Instances.getInstance(entity.path).color,
+        },
+      },
+      {
+        id: "image",
+        customComponent: CustomIconComponent,
+        visible: entity => entity.type === "ImageSeries",
+        source: entity => entity,
+        configuration: {
+          action: "clickShowImg",
+          icon: "picture-o",
+          label: "Plot",
+          tooltip: "Plot image series",
+          color: "#ffffff",
+        },
+      },
+      {
+        id: "menuOptions",
+        customComponent: ListControlsComponent,
+        source: entity => entity,
+        configuration: {
+          actions: "clickShowDetails",
+          label: "Show details",
+          tooltip: "Show details",
+          color: "#ffffff",
+        },
+      },
+
+      /*
+       * {
+       *   id: "plot",
+       *   customComponent: ColorComponent,
+       *   visible: entity =>
+       *     Instances.getInstance(entity.path + ".data")
+       *     && Instances.getInstance(entity.path + ".timestamps"),
+       *   source: entity => entity,
+       *   configuration: {
+       *     action: "clickShowPlot",
+       *     icon: "area-chart",
+       *     label: "Plot",
+       *     tooltip: "Plot time series",
+       *     defaultColor: entity => Instances.getInstance(entity.path).color,
+       *   },
+       * },
+       * {
+       *   id: "image",
+       *   customComponent: IconComponent,
+       *   visible: entity => entity.type === "ImageSeries",
+       *   source: entity => entity,
+       *   configuration: {
+       *     action: "clickShowImg",
+       *     icon: "picture-o",
+       *     label: "Plot",
+       *     tooltip: "Plot image series",
+       *   },
+       * },
+       * {
+       *   id: "addToPlot",
+       *   customComponent: AddToPlotComponent,
+       *   visible: entity =>
+       *     Instances.getInstance(entity.path + ".data")
+       *     && Instances.getInstance(entity.path + ".timestamps"),
+       *   configuration: {
+       *     icon: "gpt-addplot",
+       *     action: "clickAddToPlot",
+       *     label: "Add Plot",
+       *     tooltip: "Add plot",
+       *   },
+       * },
+       */
+    ],
+  },
   {
     id: "path",
     title: "Path",
@@ -15,70 +103,13 @@ const conf = [
   {
     id: "type",
     title: "Type",
-    source: "type"
+    source: "type",
   },
   {
     id: "description",
     title: "Description",
-    source: 'description',
+    source: "description",
   },
-  
-  {
-    id: "controls",
-    title: "Controls",
-    customComponent: GroupComponent,
-      
-    configuration: [
-      {
-        id: "showdetails",
-        customComponent: IconComponent,
-        configuration: {
-          action: "clickShowDetails",
-          icon: "info-circle",
-          label: "Show details",
-          tooltip: "Show details",
-        },
-      },
-      {
-        id: "plot",
-        customComponent: ColorComponent,
-        visible: entity => Instances.getInstance(entity.path + '.data') && Instances.getInstance(entity.path + '.timestamps'),
-        source: entity => entity,
-        configuration: {
-          action: "clickShowPlot",
-          icon: "area-chart",
-          label: "Plot",
-          tooltip: "Plot time series",
-          defaultColor: entity => Instances.getInstance(entity.path).color,
-          
-        },
-      },
-      {
-        id: "image",
-        customComponent: IconComponent,
-        visible: entity => entity.type === 'ImageSeries',
-        source: entity => entity,
-        configuration: {
-          action: "clickShowImg",
-          icon: "picture-o",
-          label: "Plot",
-          tooltip: "Plot image series"
-        },
-      },
-      {
-        id: "addToPlot",
-        customComponent: AddToPlotComponent,
-        visible: entity => Instances.getInstance(entity.path + '.data') && Instances.getInstance(entity.path + '.timestamps'),
-        configuration: {
-          icon: "gpt-addplot",
-          action: "clickAddToPlot",
-          label: "Add Plot",
-          tooltip: "Add plot",
-        }
-      }
-    ]
-  },
-  
 ];
 
 export default conf;
